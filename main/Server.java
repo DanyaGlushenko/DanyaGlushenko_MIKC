73d16d78a7fd89e0729271ed6e242d40c20e2fc0
DanyaGlushenko <155770685+DanyaGlushenko@users.noreply.github.com>
2024-01-05 21:32:42 +0200

    Add files via upload

---
+++ main/Server.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Server implements Interface{
+
+    public Server() {
+
+    }
+
+    public void send(double a) {
+        System.out.print("Received "+a+"\n");
+
+    }
+
+}
